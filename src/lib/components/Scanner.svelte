137c7a2e669ac09d066b54d2a4f29cdf1a152725
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-08-14 10:49:37 -0500

    Barcode scanner

---
+++ src/lib/components/Scanner.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import Quagga, {type QuaggaJSResultObject} from '@ericblade/quagga2';
+
+    import {onMount} from 'svelte';
+
+    let scanner: HTMLDivElement;
+    let {scanning, onScan}: {
+        scanning: boolean;
+        onScan: (code: string | null) => void;
+    } = $props();
+
+    function onDetected(data: QuaggaJSResultObject) {
+        const code = data.codeResult.code;
+        console.log('Barcode detected:', code);
+        onScan(code);
+        scanning = false;
+        Quagga.stop();
+    }
+
+    onMount(() => {
+        window.q = Quagga;
+    });
+
+    $effect(() => {
+        if (scanning) {
+            Quagga.init({
+                inputStream: {
+                    type: 'LiveStream',
+                    constraints: {
+                        facingMode: 'environment'
+                    },
+                    target: scanner
+                },
+                decoder: {
+                    readers: [
+                        'upc_reader'
+                    ]
+                }
+            }).then(() => {
+                Quagga.start();
+            });
+
+            Quagga.onDetected(onDetected);
+
+        } else {
+            Quagga.offDetected(onDetected);
+            Quagga.stop();
+        }
+    });
+</script>
+
+{#if scanning}
+<div bind:this={scanner}
+     class="fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-50"></div>
+
+<button class="btn btn-danger fixed bottom-4 right-4 z-10"
+        onclick={() => {
+            onScan(null);
+        }
+        }>
+    Cancel
+</button>
+    {/if}
