efaf078662750a7a5adbe103829cb050468836e5
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2025-08-18 15:02:14 +0000

    Remove Svelte favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
     import '../app.css';
-    import favicon from '$lib/assets/favicon.svg';
     import type {LayoutProps} from './$types';
     import { Toaster } from '@skeletonlabs/skeleton-svelte';
     import {toaster} from '$lib/toaster';
 
     let {children}: LayoutProps = $props();
 </script>
-
-<svelte:head>
-    <link href={favicon} rel="icon"/>
-</svelte:head>
 
 <Toaster {toaster}></Toaster>
 
