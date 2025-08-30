31177e142a29769215c64f21b2fc29fa954be3e0
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2025-08-30 17:02:26 +0000

    Use round & valueSweat functions for ownership calculation

--- src/lib/server/ownership.ts
+++ src/lib/server/ownership.ts
@@ -1,16 +1,8 @@
 import { Query } from 'node-appwrite';
 import { collections, listDocuments } from './database';
 import { getUserData } from './user';
-
-export function valueSweat(units: number) {
-    // Federal minimum wage
-    return units * 7.25;
-}
-
-export function normalizeShare(value: number) {
-    // Round to the 4th decimal place
-    return Math.round(value * 10000) / 10000;
-}
+import { valueSweat } from '../valueSweat';
+import { round } from '$lib/round';
 
 export async function calculateOwnership(userId: string) {
 	// Get investments for all users (and find this user's investments as well)
@@ -22,7 +14,6 @@ export async function calculateOwnership(userId: string) {
 	let totalMonetaryInvestments = 0;
 	let totalSweatInvestments = 0;
 
-
 	for (const user of allUsers) {
 		totalMonetaryInvestments += user.monetaryInvestment || 0;
 		totalSweatInvestments += user.sweatInvestment || 0;
@@ -33,8 +24,8 @@ export async function calculateOwnership(userId: string) {
 	const userMonetaryInvestment = userData.monetaryInvestment || 0;
 	const userSweatInvestment = userData.sweatInvestment || 0;
 
-    const totalValue = totalMonetaryInvestments + valueSweat(totalSweatInvestments);
-    const userValue = userMonetaryInvestment + valueSweat(userSweatInvestment);
+	const totalValue = totalMonetaryInvestments + valueSweat(totalSweatInvestments);
+	const userValue = userMonetaryInvestment + valueSweat(userSweatInvestment);
 
-    return normalizeShare(userValue / totalValue);
+	return round(userValue / totalValue);
 }
