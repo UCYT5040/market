6ddf7870cac45e522b2b045e61f17f6736797a1e
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2025-09-01 00:17:06 +0000

    Redirect users who are not logged in to the auth page

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import { Account } from 'node-appwrite';
 import { getUserData } from '$lib/server/user';
 import { CronJob } from 'cron';
 import { processMonetaryInvestments } from '$lib/server/processMonetaryInvestments';
-import type { ServerInit } from '@sveltejs/kit';
+import { redirect, type ServerInit } from '@sveltejs/kit';
 import { processDividends } from '$lib/server/processDividends';
 
 export async function handle({ event, resolve }) {
@@ -29,7 +29,7 @@ export async function handle({ event, resolve }) {
 		event.url.pathname.startsWith('/dashboard') &&
 		(!event.locals.user || !event.locals.user.approved)
 	) {
-		return new Response('Unauthorized', { status: 401 });
+		return redirect(303, '/auth');
 	}
 
 	// Require admin on /dashboard/admin endpoints
